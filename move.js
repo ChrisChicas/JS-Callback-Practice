cb71a0ea44f8ec9deb82ab1e4742a04f3a84f29b
Chris Chicas <christianchicas7@gmail.com>
2021-12-22 03:54:35 -0500

    js added + bonus

--- move.js
+++ move.js
@@ -6,7 +6,87 @@ function move(element) {
         element.style.bottom = bottom + 'px'
     }
 
+    function moveWithArrowKeys(left, bottom, callback) {
+        let direction = null;
+        let x = left;
+        let y = bottom;
+
+        element.style.left = x + "px";
+        element.style.bottom = y + "px";
+
+        function moveCharacter(){
+            if(direction === "west" && x !== 0){
+                x = x - 1;
+            }
+
+            if(direction === "north" && y !== window.innerHeight - 75){
+                y = y + 1;
+            }
+
+            if(direction === "east" && x !== window.innerWidth - 50){
+                x = x + 1;
+            }
+
+            if(direction === "south" && y !== inventory.clientHeight){
+                y = y - 1;
+            }
+            element.style.left = x + "px";
+            element.style.bottom = y + "px";
+
+            if (y < 250){
+                character.style.zIndex = 17
+            } else if (y < 350) {
+                character.style.zIndex = 13
+            } else if (y < 450) {
+                character.style.zIndex = 7
+            } else if (y < 555) {
+                character.style.zIndex = 5
+            } else if (y < 590) {
+                character.style.zIndex = 3
+            } else {
+                character.style.zIndex = 1
+            }
+            
+        }
+    
+
+        setInterval(moveCharacter, 1)
+
+        document.addEventListener("keydown", function(e){
+            if(e.repeat) return;
+            
+            if(e.key === "ArrowLeft"){
+                direction = "west"
+            }
+        
+            if(e.key === "ArrowUp"){
+                direction = "north"
+            }
+        
+            if(e.key === "ArrowRight"){
+                direction = "east"
+            }
+        
+            if(e.key === "ArrowDown"){
+                direction = "south"
+            }
+
+            if(typeof callback === "function"){
+                callback(direction);
+            }
+        })
+        
+        document.addEventListener("keyup", function(e){
+            direction = null;
+            
+            if(typeof callback === "function"){
+                callback(direction);
+            }
+        })
+    }
+
     return {
-        to: moveToCoordinates
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
     }
 }
--- newImage.js
+++ newImage.js
@@ -1,6 +1,9 @@
+let i = 0
 function newImage(url){
     let image = document.createElement('img')
     image.src = url
+    image.style.zIndex = i
+    i = i + 2
     document.body.append(image)
     return image
 }
